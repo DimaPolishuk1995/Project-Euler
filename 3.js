0a8857de0b48caec5eba54af37a48f3d0ae85cef
DimaPolishuk1995 <suvenir9502@gmail.com>
2016-06-27 16:09:03 +0300

    2.js

--- 3.js
+++ 3.js
@@ -1,3 +1,6 @@
+/*The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?*/
+
 (function LargestPrimeFactor(value) {
 	var value = 600851475143;
 	for (var i = 2; i < value; i++) {
--- 4.js
+++ 4.js
@@ -1,3 +1,8 @@
+/*A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.*/
+
+
 var palindrom = 0;
 for (var n = 999; n >=100; n=n-1) {
 	for (var i = 999; i >=100; i=i-1) {

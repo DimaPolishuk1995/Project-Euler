b1af0abd5a41cc917cf3660cb7f0e3ff86d5fd1b
DimaPolishuk1995 <suvenir9502@gmail.com>
2016-06-27 16:17:29 +0300

    4.js

--- 4.js
+++ 4.js
@@ -8,7 +8,7 @@ for (var n = 999; n >=100; n=n-1) {
 	for (var i = 999; i >=100; i=i-1) {
   	number = i*n;
     if (number == number.toString().split('').reverse().join('') && number > palindrom) {
-			 	palindromes = number;
+			 	palindrom = number;
     }
   }
 }

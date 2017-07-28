4a3da3919da9deef0864f4f9289c52b490b73b1c
sliceart_polishchuk <dpolishchuk@sliceart.com.ua>
2017-07-28 13:51:01 +0300

    change functions and update to es6

--- 1.js
+++ 1.js
@@ -1,12 +1,21 @@
 /*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
  Find the sum of all the multiples of 3 or 5 below 1000.*/
 
-(function arithmeticProgressionSum() {
-    var sum = 0;
-    for (var n = 1000; n >= 0; n--) {
-        if (n % 3 === 0 || n % 5 === 0) {
-            sum += n;
-        }
+function sumOfMultiples(number) {
+  let sum = 0;
+  for (let n = 0; n < number; n++) {
+    if (n % 3 === 0 || n % 5 === 0) {
+      sum += n;
     }
-    return sum;
-}());
+  }
+  return sum;
+}
+
+const resultOne = sumOfMultiples(10);
+console.log(resultOne); //23
+
+const resultTwo = sumOfMultiples(100);
+console.log(resultTwo); //2318
+
+const resultThree = sumOfMultiples(1000);
+console.log(resultThree); //233168
